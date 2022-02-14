728d23e923d6898e4658c02c83de024e29dc70bc
lars <lars@localhost.localdomain>
2022-02-14 15:37:32 +0100

    Worked on the dehashing algorithm. I should have paid more attention in math class.

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from encode import Encode
 import math
 
-e = Encode()
-data = e.encode("tree.jpg")
+#e = Encode()
+#data = e.encode("tree.jpg")
 
-data_list = []
+data_list = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
 first_data_list = []
 second_data_list = []
 
-for char in str(data):
-    data_list.append(char)
+#for char in str(data):
+#    data_list.append(char)
 
 for index in range(len(data_list)):
     if index % 2 == 0:
@@ -19,10 +19,37 @@ for index in range(len(data_list)):
     if index % 2 != 0:
         second_data_list.append(data_list[index])
 
+
 total_encrypted_list = first_data_list + second_data_list
+print(first_data_list)
+print(second_data_list)
+print(total_encrypted_list)
 
 
-print(len(second_data_list))
-second_half_index_slice = math.trunc(len(total_encrypted_list) / 2)
 
-first_half_index_slice = len(total_encrypted_list) - second_half_index_slice
+second_index = round(len(total_encrypted_list) / 2)
+first_index = len(total_encrypted_list) - second_index
+
+first_dehash_list = total_encrypted_list[:first_index]
+second_dehash_list = total_encrypted_list[second_index+1:]
+
+return_list = []
+
+for a in total_encrypted_list:
+    return_list.append("0")
+
+print(return_list)
+
+for ind in range(len(first_dehash_list)):
+    if ind == 0:
+        return_list.append(first_dehash_list[ind])
+    else:
+        return_list.insert(ind * 2 - 2, first_dehash_list[ind - 1])
+counter = 1
+for indy in range(len(second_dehash_list)-1):
+    return_list.insert(indy + counter, second_dehash_list[indy])
+    counter += 1
+
+
+
+print(return_list)
